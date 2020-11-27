de95f4dc1842c8075fa8b46d099a2e0d6ef56d49
SAMcGee <57569081+SAMcGee@users.noreply.github.com>
2020-11-27 13:22:52 +0000

    Update image2.py

--- ivr_assignment/src/image2.py
+++ ivr_assignment/src/image2.py
@@ -46,10 +46,6 @@ class image_converter:
         self.robot_joint2_angle_estimate_pub = rospy.Publisher("/robot/joint2_angle_est", Float64, queue_size=10)
         self.robot_joint3_angle_estimate_pub = rospy.Publisher("/robot/joint3_angle_est", Float64, queue_size=10)
         self.robot_joint4_angle_estimate_pub = rospy.Publisher("/robot/joint4_angle_est", Float64, queue_size=10)
-        
-        # Publishers for joint positions
-        self.robot_red_blob_pub = rospy.Publisher("/robot/red_blob_position", Float64MultiArray, queue_size=10)
-        self.robot_yellow_blob_pub = rospy.Publisher("/robot/yellow_blob_position", Float64MultiArray, queue_size=10)
 
         # Subscribers to recieve both images
         self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback1)
@@ -75,17 +71,12 @@ class image_converter:
         self.move_robot()
         
         self.joints = Float64MultiArray
-        self.red_blob_loc = Float64MultiArray
-        self.yellow_blob_loc = Float64MultiArray
         
         self.joints.data = self.detect_joint_angles(self.cv_image1, self.cv_image2)
 
         # Publish the results
         try:
             self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
-            
-            self.robot_red_blob_pub.publish(self.red_blob_loc)
-            self.yellow_blob_loc.publish(self.yellow_blob_loc)
             
             self.robot_joint2_angle_estimate_pub.publish(self.joints.data[0])
             self.robot_joint3_angle_estimate_pub.publish(self.joints.data[1])
@@ -167,10 +158,6 @@ class image_converter:
         blueCoordinates = np.array([im2_blue[0], im1_blue[0], im2_blue[1]])
         greenCoordinates = np.array([im2_green[0], im1_green[0], im2_green[1]])
         redCoordinates = np.array([im2_red[0], im1_red[0], im2_red[1]])
-        
-        # Stores blob positions to publish for use in FK calculations 
-        self.red_blob_loc = redCoordinates
-        self.yellow_blob_loc = yellowCoordinates
 
         # ja2 rotates in x (ja1 doesn't rotate)
         # The 0.2 is an error value as the joint is consistenly overstimated by 0.2 radians
