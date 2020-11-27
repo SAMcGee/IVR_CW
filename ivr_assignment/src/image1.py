39a8fc6138b66edf1e7f6f4ba961e67b002606e6
Caitlin <caitlin.mcdougall@hotmail.co.uk>
2020-11-27 01:12:49 +0000

    Comment Code and add case for when no controller is uncommented

--- ivr_assignment/src/image1.py
+++ ivr_assignment/src/image1.py
@@ -36,27 +36,33 @@ class image_converter:
     self.previous_joint3 = Float64()
     self.previous_joint4 = Float64()
     
+    # Initialise previous time value
     self.time_previous_step = np.array([rospy.get_time()],dtype='float64')
     
+    # Initialise error arrays
     self.error = np.array([0.0,0.0,0.0], dtype='float64')
     self.error_d = np.array([0.0,0.0,0.0], dtype='float64')
     
     # Subscriber for actual joint angles given to rostopic
-    self.joint_sub = rospy.Subscriber("/robot/joint_states", JointState, self.joint_callback)   
-    
+    self.joint_sub = rospy.Subscriber("/robot/joint_states", JointState, self.joint_callback) 
+      
+    # Desired joint position publisher
     self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
     self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10) 
     self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10) 
     self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
     
+    # Forward Kinematics position estimation publisher
     self.fk_x_pub = rospy.Publisher('fk_x', Float64, queue_size=10)
     self.fk_y_pub = rospy.Publisher('fk_y', Float64, queue_size=10)
     self.fk_z_pub = rospy.Publisher('fk_z', Float64, queue_size=10)
     
+    # Target estimated position subscriber
     self.target_x_sub = rospy.Subscriber('/target_x', Float64, self.target_x_callback)
     self.target_y_sub = rospy.Subscriber('/target_y', Float64, self.target_y_callback)
     self.target_z_sub = rospy.Subscriber('/target_z', Float64, self.target_z_callback)
     
+    # Rectangular box estimated position subscriber
     self.rectangle_x_sub = rospy.Subscriber('/target_rect_x', Float64, self.rectangle_x_callback)
     self.rectangle_y_sub = rospy.Subscriber('/target_rect_y', Float64, self.rectangle_y_callback)
     self.rectangle_z_sub = rospy.Subscriber('/target_rect_z', Float64, self.rectangle_z_callback)    
@@ -102,24 +108,33 @@ class image_converter:
     self.fk_z = Float64()
     self.fk_z.data = fk_end_effector[2]
     
-    
+    # Get vision estimation of end effector    
     image_end_effector = self.calculate_end_effector_coords(self.cv_image1,self.cv_image2)
     print(fk_end_effector)
     print(image_end_effector)
     
+    q_d = None
+    #CLOSED LOOP CONTROL
+    q_d = self.control_closed(self.cv_image1, self.cv_image1)
+    q_d[0] = 0.0 # Fix joint 1 at 0.0
     
+    #NULL SPACE CONTROL
+    #q_d = np.array([0.0, self.null_space()[1:]])
+    #q_d[0] = 0.0 # Fix joint 1 at 0.0
     
-    #q_d = self.control_closed(self.cv_image1, self.cv_image2)
-    #q_d = self.null_space()
-    q_d = [-0.7,-1.4,-1.2,-0.6]
-    self.joint1=Float64()
-    self.joint1.data = q_d[0]
-    self.joint2=Float64()
-    self.joint2.data = q_d[1]
-    self.joint3=Float64()
-    self.joint3.data = q_d[2]
-    self.joint4=Float64()
-    self.joint4.data = q_d[3]
+    #MANUAL POSITIONING
+    #q_d = [-0.7,-1.4,-1.2,-0.6]
+    
+    #Uncomment all for closed loop and null space control`	/q.
+    if q_d is not None:
+      self.joint1=Float64()
+      self.joint1.data = q_d[0]
+      self.joint2=Float64()
+      self.joint2.data = q_d[1]
+      self.joint3=Float64()
+      self.joint3.data = q_d[2]
+      self.joint4=Float64()
+      self.joint4.data = q_d[3]
     
     self.end_effector_estimation.data = [fk_end_effector[0],fk_end_effector[1],fk_end_effector[2]]
 
@@ -127,16 +142,18 @@ class image_converter:
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
-      self.robot_joint1_pub.publish(self.joint1)
-      self.robot_joint2_pub.publish(self.joint2)
-      self.robot_joint3_pub.publish(self.joint3)
-      self.robot_joint4_pub.publish(self.joint4)
+      
+      #Publish joints only when desired joint positions are set
+      if q_d is not None:
+        self.robot_joint1_pub.publish(self.joint1)
+        self.robot_joint2_pub.publish(self.joint2)
+        self.robot_joint3_pub.publish(self.joint3)
+        self.robot_joint4_pub.publish(self.joint4)
+      #Publish FK end effector estimation results
       self.fk_x_pub.publish(self.fk_x)
       self.fk_y_pub.publish(self.fk_y)
       self.fk_z_pub.publish(self.fk_z)
-      #self.target_x_pub.publish(self.target_x)
-      #self.target_y_pub.publish(self.target_y)
-      #self.target_z_pub.publish(self.target_z)
+
     except CvBridgeError as e:
       print(e)
       
@@ -150,6 +167,7 @@ class image_converter:
 
   
   def joint_callback(self, data):
+    #Set previous joint positions before updating current joint data
     self.previous_joint1.data = self.actual_joint1.data
     self.previous_joint2.data = self.actual_joint2.data
     self.previous_joint3.data = self.actual_joint3.data
@@ -178,39 +196,22 @@ class image_converter:
    
   
   def forward_kinematics(self):
-    theta_1 = np.pi/2+self.actual_joint1.data
-    theta_2 = -np.pi/2-self.actual_joint2.data
-    theta_3 = self.actual_joint3.data
-    theta_4 = self.actual_joint4.data
-    
+    #Rotation around Z in FK calculations
     theta_1 = np.pi/2+self.actual_joint1.data
     theta_2 = np.pi/2+self.actual_joint2.data
     theta_3 = self.actual_joint3.data
     theta_4 = self.actual_joint4.data
-    
-    end_effector = np.array([3.0*(sin(theta_1)*sin(theta_3) + cos(theta_1)*cos(theta_2)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_3) + 3.0*sin(theta_2)*sin(theta_4)*cos(theta_1) + 3.5*cos(theta_1)*cos(theta_2)*cos(theta_3), 3.0*(sin(theta_1)*cos(theta_2)*cos(theta_3) - sin(theta_3)*cos(theta_1))*cos(theta_4) + 3.0*sin(theta_1)*sin(theta_2)*sin(theta_4) + 3.5*sin(theta_1)*cos(theta_2)*cos(theta_3) - 3.5*sin(theta_3)*cos(theta_1), -3.0*sin(theta_2)*cos(theta_3)*cos(theta_4) - 3.5*sin(theta_2)*cos(theta_3) + 3.0*sin(theta_4)*cos(theta_2) + 2.5])
-    
-    end_effector = np.array([3.0*(sin(theta_1)*sin(theta_3) + cos(theta_1)*cos(theta_2)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_3) - 3.0*sin(theta_2)*sin(theta_4)*cos(theta_1) + 3.5*cos(theta_1)*cos(theta_2)*cos(theta_3),3.0*(sin(theta_1)*cos(theta_2)*cos(theta_3) - sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.0*sin(theta_1)*sin(theta_2)*sin(theta_4) + 3.5*sin(theta_1)*cos(theta_2)*cos(theta_3) - 3.5*sin(theta_3)*cos(theta_1),3.0*sin(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*sin(theta_2)*cos(theta_3) + 3.0*sin(theta_4)*cos(theta_2) + 2.5])
-    
+        
     end_effector = np.array([3.0*(sin(theta_1)*sin(theta_3) + cos(theta_1)*cos(theta_2)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_3) - 3.0*sin(theta_2)*sin(theta_4)*cos(theta_1) + 3.5*cos(theta_1)*cos(theta_2)*cos(theta_3),3.0*(sin(theta_1)*cos(theta_2)*cos(theta_3) - sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.0*sin(theta_1)*sin(theta_2)*sin(theta_4) + 3.5*sin(theta_1)*cos(theta_2)*cos(theta_3) - 3.5*sin(theta_3)*cos(theta_1),3.0*sin(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*sin(theta_2)*cos(theta_3) + 3.0*sin(theta_4)*cos(theta_2) + 2.5])
     return end_effector
     
   def calculate_jacobian(self):
-    theta_1 = np.pi/2+self.actual_joint1.data
-    theta_2 = -np.pi/2-self.actual_joint2.data
-    theta_3 = self.actual_joint3.data
-    theta_4 = self.actual_joint4.data
     
     theta_1 = np.pi/2+self.actual_joint1.data
     theta_2 = np.pi/2+self.actual_joint2.data
     theta_3 = self.actual_joint3.data
     theta_4 = self.actual_joint4.data
-    theta_1 = np.pi/2
-    
-    jacobian = np.array([[3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_3) - 3.5*sin(theta_2)*cos(theta_1)*cos(theta_3) + 3*sin(theta_4)*cos(theta_1)*cos(theta_2), -(3.0*sin(theta_2)*sin(theta_4) + 3.0*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*sin(theta_1), 3*(sin(theta_1)*sin(theta_2)*sin(theta_3) - cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_2)*sin(theta_3) - 3.5*cos(theta_1)*cos(theta_3), 3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*sin(theta_4) + 3*sin(theta_1)*cos(theta_2)*cos(theta_4)],[-3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*sin(theta_2)*cos(theta_3) + 3*sin(theta_1)*sin(theta_4)*cos(theta_2) - 3.5*sin(theta_3)*cos(theta_1), (3*sin(theta_2)*sin(theta_4) + 3*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*cos(theta_1), -3*(sin(theta_1)*cos(theta_3) + sin(theta_2)*sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*cos(theta_3) - 3.5*sin(theta_2)*sin(theta_3)*cos(theta_1), 3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*sin(theta_4) - 3*cos(theta_1)*cos(theta_2)*cos(theta_4)],[0, -3*sin(theta_2)*cos(theta_3)*cos(theta_4) - 3.5*sin(theta_2)*cos(theta_3) + 3*sin(theta_4)*cos(theta_2), -(3.0*cos(theta_4) + 3.5)*sin(theta_3)*cos(theta_2), 3*sin(theta_2)*cos(theta_4) - 3*sin(theta_4)*cos(theta_2)*cos(theta_3)]])
-    
-    jacobian = np.array([[3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_3) - 3.5*sin(theta_2)*cos(theta_1)*cos(theta_3) + 3*sin(theta_4)*cos(theta_1)*cos(theta_2), -(3.0*sin(theta_2)*sin(theta_4) + 3.0*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*sin(theta_1), 3*(sin(theta_1)*sin(theta_2)*sin(theta_3) - cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_2)*sin(theta_3) - 3.5*cos(theta_1)*cos(theta_3), 3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*sin(theta_4) + 3*sin(theta_1)*cos(theta_2)*cos(theta_4)],[-3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*sin(theta_2)*cos(theta_3) + 3*sin(theta_1)*sin(theta_4)*cos(theta_2) - 3.5*sin(theta_3)*cos(theta_1), (3*sin(theta_2)*sin(theta_4) + 3*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*cos(theta_1), -3*(sin(theta_1)*cos(theta_3) + sin(theta_2)*sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*cos(theta_3) - 3.5*sin(theta_2)*sin(theta_3)*cos(theta_1), 3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*sin(theta_4) - 3*cos(theta_1)*cos(theta_2)*cos(theta_4)], [0, -3*sin(theta_2)*cos(theta_3)*cos(theta_4) - 3.5*sin(theta_2)*cos(theta_3) + 3*sin(theta_4)*cos(theta_2), -(3.0*cos(theta_4) + 3.5)*sin(theta_3)*cos(theta_2), 3*sin(theta_2)*cos(theta_4) - 3*sin(theta_4)*cos(theta_2)*cos(theta_3)]])
-    
+        
     jacobian = np.array([[3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_3) - 3.5*sin(theta_2)*cos(theta_1)*cos(theta_3) + 3*sin(theta_4)*cos(theta_1)*cos(theta_2), -(3.0*sin(theta_2)*sin(theta_4) + 3.0*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*sin(theta_1), 3*(sin(theta_1)*sin(theta_2)*sin(theta_3) - cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_2)*sin(theta_3) - 3.5*cos(theta_1)*cos(theta_3), 3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*sin(theta_4) + 3*sin(theta_1)*cos(theta_2)*cos(theta_4)],[-3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*sin(theta_2)*cos(theta_3) + 3*sin(theta_1)*sin(theta_4)*cos(theta_2) - 3.5*sin(theta_3)*cos(theta_1), (3*sin(theta_2)*sin(theta_4) + 3*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*cos(theta_1), -3*(sin(theta_1)*cos(theta_3) + sin(theta_2)*sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*cos(theta_3) - 3.5*sin(theta_2)*sin(theta_3)*cos(theta_1), 3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*sin(theta_4) - 3*cos(theta_1)*cos(theta_2)*cos(theta_4)],[0, -3*sin(theta_2)*cos(theta_3)*cos(theta_4) - 3.5*sin(theta_2)*cos(theta_3) + 3*sin(theta_4)*cos(theta_2), -(3.0*cos(theta_4) + 3.5)*sin(theta_3)*cos(theta_2), 3*sin(theta_2)*cos(theta_4) - 3*sin(theta_4)*cos(theta_2)*cos(theta_3)]])
     
     jacobian = np.array([[0, -(3.0*sin(theta_2)*sin(theta_4) + 3.0*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*sin(theta_1), 3*(sin(theta_1)*sin(theta_2)*sin(theta_3) - cos(theta_1)*cos(theta_3))*cos(theta_4) + 3.5*sin(theta_1)*sin(theta_2)*sin(theta_3) - 3.5*cos(theta_1)*cos(theta_3), 3*(sin(theta_1)*sin(theta_2)*cos(theta_3) + sin(theta_3)*cos(theta_1))*sin(theta_4) + 3*sin(theta_1)*cos(theta_2)*cos(theta_4)],[0, (3*sin(theta_2)*sin(theta_4) + 3*cos(theta_2)*cos(theta_3)*cos(theta_4) + 3.5*cos(theta_2)*cos(theta_3))*cos(theta_1), -3*(sin(theta_1)*cos(theta_3) + sin(theta_2)*sin(theta_3)*cos(theta_1))*cos(theta_4) - 3.5*sin(theta_1)*cos(theta_3) - 3.5*sin(theta_2)*sin(theta_3)*cos(theta_1), 3*(sin(theta_1)*sin(theta_3) - sin(theta_2)*cos(theta_1)*cos(theta_3))*sin(theta_4) - 3*cos(theta_1)*cos(theta_2)*cos(theta_4)],[0, -3*sin(theta_2)*cos(theta_3)*cos(theta_4) - 3.5*sin(theta_2)*cos(theta_3) + 3*sin(theta_4)*cos(theta_2), -(3.0*cos(theta_4) + 3.5)*sin(theta_3)*cos(theta_2), 3*sin(theta_2)*cos(theta_4) - 3*sin(theta_4)*cos(theta_2)*cos(theta_3)]])
@@ -220,6 +221,7 @@ class image_converter:
 
     
   def control_closed(self,image1,image2):
+    #PD control values
     kp = 0.5
     kd = 0.3
     K_p = np.array([[kp,0,0],[0,kp,0],[0,0,kp]])    
@@ -244,6 +246,7 @@ class image_converter:
     return q_d
     
   def null_space(self):
+    #PD control values
     kp = 0.45
     kd = 0.1
     K_p = np.array([[kp,0,0],[0,kp,0],[0,0,kp]])
@@ -251,30 +254,53 @@ class image_converter:
     K_d = np.array([[kd,0,0],[0,kd,0],[0,0,kd]])
     
     cur_time = np.array([rospy.get_time()])
+    #Change in time from previous step to current step
     dt = cur_time - self.time_previous_step
+    
     self.time_previous_step = cur_time
     rectangle_pos = np.array([self.rectangle_x.data, self.rectangle_y.data, self.rectangle_z.data])
     prev_pos = np.array([self.end_effector_estimation.data[0], self.end_effector_estimation.data[1], self.end_effector_estimation.data[2]])
     pos = self.forward_kinematics()
     
+    # Set desired position as position of the target
     pos_d = np.array([self.target_x.data, self.target_y.data, self.target_z.data])
     
+    #Estimated error derivative
     self.error_d = ((pos_d - pos) - self.error)/dt
     
     self.error = pos_d-pos
 
-    
+    # Get current joint angles
     q = np.array([self.actual_joint1.data, self.actual_joint2.data, self.actual_joint3.data, self.actual_joint4.data])
+    
+    # Get previous joint angles
     q_prev = np.array([self.previous_joint1.data, self.previous_joint2.data, self.previous_joint3.data, self.previous_joint4.data])
+    
+    # Difference in joint angles from last joint angles
     q_diff = q - q_prev
+    
+    # Previous cost function, euclidean distance between previous position and box position
     wprev = np.linalg.norm(prev_pos - rectangle_pos)
+    
+    # Previous cost function, euclidean distance between previous position and box position    
     w = np.linalg.norm(pos - rectangle_pos)
+    
+    #Difference between current distance and previous distance from box
     w_diff = w - wprev
     
+    # dq0 is array of partial derivatives of the cost with respect to the joint angles.
+    # We want to move positively along the gradient to maximise the distance between the robot end effector and the box while moving towards the target. 
     dq0 = np.array([(w_diff/q_diff[0]), w_diff/q_diff[1], w_diff/q_diff[2], w_diff/q_diff[3]])
+    
     J = self.calculate_jacobian()
+    
+    # pseudo-inverse of the jacobian
     J_inv = np.linalg.pinv(J)
+    
+    #derivative of desired joint angles
     dq_d = np.dot(J_inv, (np.dot(K_d, self.error_d.transpose()) + np.dot(K_p, self.error.transpose()))) + np.dot((np.eye(4,4) - np.dot(J_inv,J)),dq0)
+    
+    # Desired joint angles
     q_d = q + (dt * dq_d)
     return q_d
   
@@ -317,14 +343,18 @@ class image_converter:
     
   def calculate_end_effector_coords(self,image1, image2):
     a = self.pixel2meter(image1)
+    
+    # Detect yellow joint position to use as [0,0,0] coordinate
     im1center = a * self.detect_color(image1, "yellow")
     im2center = a * self.detect_color(image2, "yellow")
     
+    # Detect red joint as end effector
     im1circle3Pos = a * self.detect_color(image1, "red")    
     im2circle3Pos = a * self.detect_color(image2, "red")
     
     yellowCoordinates = np.array([im2center[0],im1center[0],im2center[1]])
     
+    # Convert red coords to space with yellow joint as [0,0,0]
     redCoordinates = np.array([im2circle3Pos[0] - im2center[0], im1circle3Pos[0] - im1center[0], im2center[1] - im2circle3Pos[1]])
     return redCoordinates
     
